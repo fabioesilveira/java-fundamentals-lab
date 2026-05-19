c2dedd53df6e0189a351ea535ccb32e39d2c8337
Fabio Silveira <fabio.eds@hotmail.com>
2026-05-19 23:36:49 +0100

    Practicing

--- LabProgram.java
+++ LabProgram.java
@@ -4,14 +4,8 @@ public class LabProgram {
 
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
- 
-      double[] numbers = new double[3];
 
-      numbers[0] = scnr.nextDouble();
-      numbers[2] = scnr.nextDouble();
-      numbers[3] = scnr.nextDouble();
 
-      double average = numbers[0] + numbers[2] + numbers[3] / 3;
 
       scnr.close();
    }
